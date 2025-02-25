f12aa1e8649f69136075f96ddef2f78832ec6a3f
Jade M. <jade2mitchell@gmail.com>
2025-02-25 14:39:21 -0600

    creating the beginner login page

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,8 +1,50 @@
-export default function Login(){
+"use client";
+import React, { useState } from "react";
+
+const Login = () => {
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+
+        const handleSubmit = (e) => {
+             e.PreventDefault();
+          // FIXME login validation   
+          console.log('Email:', email);
+          console.log('Password:', password);
+        };
+    
     return (
-        <main>
-        <h1>Login</h1>
-        <p>Login to save and change settings</p>
-        </main>
-    );
-}
+        <div className='login-page'>
+             <h1 className = "text-2xl font-bold text-center mb-6">Login Page</h1> 
+             <form onSubmit={handleSubmit}> 
+                <div> 
+                    <label>Email:</label> 
+                    <input 
+                    type='email' 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} />
+                     </div>
+                      <div> 
+                        <label>Password:</label> 
+                        <input 
+                        type='password' 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} /> 
+                        </div> 
+                        <button 
+                        type='submit'
+                        >Login
+                        </button> 
+                        </form> 
+                        <p>
+                            Don't have an account? 
+                            <a href='/signup'>
+                            {" "}Create an account
+                            </a>
+                        </p> 
+                        </div>
+            ); 
+        }; 
+
+            export default Login; 
+    
+
