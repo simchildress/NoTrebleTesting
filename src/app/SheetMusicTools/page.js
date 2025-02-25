809a084b73664799302ed507bb226700cfd2c1f1
TrinhTT8 <ttiffany1608@gmail.com>
2025-02-25 03:24:36 -0600

    Create a layout for the Sheet Music Tools page

--- src/app/SheetMusicTools/page.js
+++ src/app/SheetMusicTools/page.js
@@ -1,7 +1,25 @@
 export default function SheetMusicTools(){
     return (
         <main>
-        <h1>SheetMusicTools</h1>
+            <h1>Sheet Music Reader</h1>
+            <button>Upload file</button>
+            <p>Piece Title: </p>
+            <p>Key: </p>
+            <p>Time Signature: </p>
+            <p>Composer: </p>
+
+            <br />
+
+            <h1>Sheet Music Composer</h1>
+            <form>
+                <label>Enter Title: <input name="title"></input></label>
+                <br />
+                <label>Enter Composer Name: <input name="title"></input></label>
+                <br />
+                <label>Enter Key: <input name="title"></input></label>
+                <br />
+                <label>Enter Time Signature: <input name="title"></input></label>
+            </form>
         </main>
     );
 }
