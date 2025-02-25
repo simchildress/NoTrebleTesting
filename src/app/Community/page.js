8202cdd02126a76965d283b623b00a8ed77605f1
TrinhTT8 <ttiffany1608@gmail.com>
2025-02-25 03:36:07 -0600

    Create a simple layout of the posting function for the Community page

--- src/app/Community/page.js
+++ src/app/Community/page.js
@@ -1,7 +1,15 @@
 export default function Community(){
     return (
         <main>
-        <h1>Community</h1>
+            <div className="create_container">
+                <h1>Community Posts</h1>
+                    <label>Let's share what going...</label>
+                        <input
+                            type="text"
+                            name="post"
+                        />
+                    <button>CREATE POST</button>
+            </div>
         </main>
     );
 }
--- src/app/SheetMusicTools/page.js
+++ src/app/SheetMusicTools/page.js
@@ -1,25 +1,29 @@
 export default function SheetMusicTools(){
     return (
         <main>
-            <h1>Sheet Music Reader</h1>
-            <button>Upload file</button>
-            <p>Piece Title: </p>
-            <p>Key: </p>
-            <p>Time Signature: </p>
-            <p>Composer: </p>
+            <div className="reader_container">
+                <h1>Sheet Music Reader</h1>
+                <button>Upload file</button>
+                <p>Piece Title: </p>
+                <p>Key: </p>
+                <p>Time Signature: </p>
+                <p>Composer: </p>
+            </div>
 
             <br />
-
-            <h1>Sheet Music Composer</h1>
-            <form>
-                <label>Enter Title: <input name="title"></input></label>
-                <br />
-                <label>Enter Composer Name: <input name="title"></input></label>
-                <br />
-                <label>Enter Key: <input name="title"></input></label>
-                <br />
-                <label>Enter Time Signature: <input name="title"></input></label>
-            </form>
+            
+            <div className="composer_container">
+                <h1>Sheet Music Composer</h1>
+                <form>
+                    <label>Enter Title: <input name="title"></input></label>
+                    <br />
+                    <label>Enter Composer Name: <input name="title"></input></label>
+                    <br />
+                    <label>Enter Key: <input name="title"></input></label>
+                    <br />
+                    <label>Enter Time Signature: <input name="title"></input></label>
+                </form>
+            </div>
         </main>
     );
 }
