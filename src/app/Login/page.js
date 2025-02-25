e0cf4b536ee6be86f1a2645382373b04b1404a81
Jade M. <jade2mitchell@gmail.com>
2025-02-25 17:25:13 -0600

    updating the login page to look more like prototype

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,46 +1,53 @@
 "use client";
 import React, { useState } from "react";
+import Link from "next/link";
 
 const Login = () => {
         const [email, setEmail] = useState('');
         const [password, setPassword] = useState('');
 
         const handleSubmit = (e) => {
-             e.PreventDefault();
-          // FIXME login validation   
+          e.preventDefault();
+          // add login validation  
           console.log('Email:', email);
           console.log('Password:', password);
+
+          //clear after submission
+          setEmail("");
+          setPassword("");
         };
     
     return (
-        <div className='login-page'>
-             <h1 className = "text-2xl font-bold text-center mb-6">Login Page</h1> 
-             <form onSubmit={handleSubmit}> 
-                <div> 
-                    <label>Email:</label> 
+        <div className=' flex flex-col items-center justify center min-h-screen bg-gray-100 p-6'>
+             <form onSubmit={handleSubmit} className="bg-white p-10 rounded-lg shadow-lg w-full max-w-2xl text-2xl "> 
+                <div className="mb-6"> 
+                <h1 className = "text-5xl font-bold text-center mb-6">Login</h1>
+                    <label className="block text-gray-800 font-semibold mb-2">Email: </label> 
                     <input 
                     type='email' 
                     value={email} 
-                    onChange={(e) => setEmail(e.target.value)} />
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full p-1 border border-gray-500 rounded "/>
                      </div>
                       <div> 
-                        <label>Password:</label> 
+                        <label className="block text-gray-800 font-semibold mb-2">Password:</label> 
                         <input 
                         type='password' 
                         value={password} 
-                        onChange={(e) => setPassword(e.target.value)} /> 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        className="w-full p-1 border border-gray-500 rounded"/> 
                         </div> 
                         <button 
-                        type='submit'
+                        type="submit" className="mt-6 w-full bg-[#455090] text-white py-4 rounded-lg hover:bg-[#102437] mt-6 text-2xl font-bold"
                         >Login
                         </button> 
                         </form> 
-                        <p>
+                        <p className="text-center mb-6 text-xl" >
                             Don't have an account? 
-                            <a href='/signup'>
-                            {" "}Create an account
-                            </a>
-                        </p> 
+                            <Link href="/Signup" className="test-blue-600 font-semibold hover:underline">
+                            {" "} Create an account
+                            </Link>
+                            </p> 
                         </div>
             ); 
         }; 
