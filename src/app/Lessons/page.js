305dcb8a016f5fbf0ce0f56a8753db770ee35e05
Jade M. <jade2mitchell@gmail.com>
2025-02-25 18:49:27 -0600

    beginner base for the lessons tab

--- src/app/Lessons/page.js
+++ src/app/Lessons/page.js
@@ -1,7 +1,35 @@
 export default function Lessons(){
+    
+    const Columns = () =>
+    <div className=" w-full bg-gray-100 h-100 grid grid-cols-3 bg-gray-200 shadow-sm p-10 rounded-lg">
+    <div className="col-span-1 text-bold text-4xl ">Scales</div>
+    <div className="col-span-1 text-bold text-4xl ">Rhythm</div>
+    <div className="col-span-1 text-bold text-4xl ">Dynamics</div>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 1</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    </div>
+
+    
     return (
         <main>
-        <h1>Lessons</h1>
+        <h1 className="text-center font-bold text-2xl">Music Theory Lessons</h1>
+        <Columns/>
         </main>
     );
 }
