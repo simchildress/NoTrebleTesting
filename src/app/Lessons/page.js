fea409d15bf27706b66cb3661f75dae09a3d80b1
Jade M. <jade2mitchell@gmail.com>
2025-02-25 19:38:24 -0600

    adding onto lessons page

--- src/app/Lessons/page.js
+++ src/app/Lessons/page.js
@@ -1,28 +1,34 @@
 export default function Lessons(){
     
     const Columns = () =>
-    <div className=" w-full bg-gray-100 h-100 grid grid-cols-3 bg-gray-200 shadow-sm p-10 rounded-lg">
-    <div className="col-span-1 text-bold text-4xl ">Scales</div>
-    <div className="col-span-1 text-bold text-4xl ">Rhythm</div>
-    <div className="col-span-1 text-bold text-4xl ">Dynamics</div>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <div className=" w-full bg-gray-200 h-100 grid grid-cols-3 bg-gray-200 shadow-lg p-10 rounded-lg">
+    <div style={{ transform: 'translateX(25px)' }} className="col-span-1 font-bold text-3xl ">Scales</div>
+    <div style={{ transform: 'translateX(20px)' }} className="col-span-1 font-bold text-3xl ">Rhythm</div>
+    <div style={{ transform: 'translateX(10px)' }} className="col-span-1 font-bold text-3xl ">Dynamics</div>
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 1</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
-    <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
     rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
     </div>
 
     
