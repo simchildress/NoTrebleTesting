dea5434dfb1a5b5efc71febabf344e154a5838a0
Jade M. <jade2mitchell@gmail.com>
2025-02-28 16:00:23 -0600

    updating the lessons page to look nicer

--- src/app/Lessons/page.js
+++ src/app/Lessons/page.js
@@ -2,41 +2,31 @@ import LevelButton from "../component/LevelButton"
 
 export default function Lessons(){
     const Columns = () =>
-    <div className=" w-full bg-gray-200 h-100 grid grid-cols-3 bg-gray-200 shadow-lg p-10 rounded-lg">
-    <div style={{ transform: 'translateX(25px)' }} className="col-span-1 font-bold text-3xl ">Scales</div>
-    <div style={{ transform: 'translateX(20px)' }} className="col-span-1 font-bold text-3xl ">Rhythm</div>
-    <div style={{ transform: 'translateX(10px)' }} className="col-span-1 font-bold text-3xl ">Dynamics</div>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 1</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
-    <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    <div className="grid grid-cols-3 mt-4">
+    <div className="col-span-1 font-bold text-3xl text-center">Scales</div>
+    <div className="col-span-1 font-bold text-3xl text-center">Rhythm</div>
+    <div className="col-span-1 font-bold text-3xl text-center">Dynamics</div>
+    <LevelButton level = {1} ></LevelButton>
+    <LevelButton level = {1} ></LevelButton>
+    <LevelButton level = {1} ></LevelButton>
+    <LevelButton level = {2} ></LevelButton>
+    <LevelButton level = {2} ></LevelButton>
+    <LevelButton level = {2} ></LevelButton>
+    <LevelButton level = {3} ></LevelButton>
+    <LevelButton level = {3} ></LevelButton>
+    <LevelButton level = {3} ></LevelButton>
+    <LevelButton level = {4} ></LevelButton>
+    <LevelButton level = {4} ></LevelButton>
+    <LevelButton level = {4} ></LevelButton>
     </div>
 
     
     return (
         <main>
-        <h1 className="text-center font-bold text-2xl">Music Theory Lessons</h1>
+        <h1 className="text-center font-bold text-4xl mt-6">Music Theory Lessons</h1>    
+        <div className= "mb-6 bg-gray-200 p-10 rounded-lg shadow-lg w-lg mt-4 ">
         <Columns/>
+        </div>
         </main>
     );
 }
--- src/app/component/LevelButton.jsx
+++ src/app/component/LevelButton.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 
 function LevelButton(props) {
-  return <button className="col-span-1 text-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4 ">Level {props.level}</button>;
-}
+  return <button className="col-span-1 text-semibold text-2xl w-60 bg-[#455090] text-white py-2  
+    rounded-lg hover:bg-[#102437] shadow-md "
+    style={{ display: 'block', margin: 'auto', marginBottom: '20px', marginTop: '10px', height: '10vh'}}>Level {props.level}</button>
+};
 export default LevelButton
