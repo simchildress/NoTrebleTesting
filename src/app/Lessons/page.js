da57bd623d588a8c0014dce7d1fda38858da9d87
Elisabeth Nguyen <elisabethnguyen@my.unt.edu>
2025-03-02 17:21:41 -0600

    add lesson 1 sample

--- src/app/Lessons/page.js
+++ src/app/Lessons/page.js
@@ -1,35 +1,36 @@
 import LevelButton from "../component/LevelButton"
+import Link from "next/link";
 
 export default function Lessons(){
     const Columns = () =>
     <div className=" w-full bg-gray-200 h-100 grid grid-cols-3 bg-gray-200 shadow-lg p-10 rounded-lg">
-    <div style={{ transform: 'translateX(25px)' }} className="col-span-1 font-bold text-3xl ">Scales</div>
+    <div style={{ transform: 'translateX(25px)' }} className="col-span-1 font-bold text-3xl ">Fundamentals</div>
     <div style={{ transform: 'translateX(20px)' }} className="col-span-1 font-bold text-3xl ">Rhythm</div>
     <div style={{ transform: 'translateX(10px)' }} className="col-span-1 font-bold text-3xl ">Dynamics</div>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
+    rounded-lg hover:bg-[#102437] mt-4 "><Link href="./Lessons/Fundamentals">Level 1</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4 ">Level 1</button>
+    rounded-lg hover:bg-[#102437] mt-4 "><Link href="./Lessons/Fundamentals">Level 1</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 1</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 1</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 2</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 2</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 2</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 2</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 3</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 3</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 3</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 3</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 4</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 4</Link></button>
     <button className="col-span-1 font-semibold text-2xl w-40 bg-[#455090] text-white py-2  
-    rounded-lg hover:bg-[#102437] mt-4">Level 4</button>
+    rounded-lg hover:bg-[#102437] mt-4"><Link href="./Lessons/Fundamentals">Level 4</Link></button>
     </div>
 
     
