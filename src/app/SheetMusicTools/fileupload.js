9209122d3a20753009c2b70d7b89f6fa08ed4c40
srah-law <smlawt03@gmail.com>
2025-04-15 12:56:23 -0500

    refined error message

--- src/app/SheetMusicTools/fileupload.js
+++ src/app/SheetMusicTools/fileupload.js
@@ -6,14 +6,13 @@ export default function FileUploader({ setFile }) {
         if (!selectedFile) return;
         
         // Only accept png, jpeg, jpg, and pdf files
-        const validTypes = ["image/png", "image/jpeg", "image/jpg", "application/pdf", "application/zip","application/xml", "application/octet-stream"]; //octet stream for
+        const validTypes = ["image/png", "image/jpeg", "image/jpg", "application/pdf","application/xml"]; 
         if (!(selectedFile.name.endsWith("mxl") || selectedFile.name.endsWith("xml"))){
 
             if (!validTypes.includes(selectedFile.type))  {
-                console.log("FUCK");
                 console.log("Selected file:", selectedFile);
                 console.log("File type:", selectedFile.type);
-                alert(`Invalid file type ${selectedFile.type}. Please upload an image or a PDF.`);
+                alert(`Invalid file type ${selectedFile.type}. Please upload an image, xml, mxl, or a PDF.`);
                 return;
             }
 
@@ -29,7 +28,7 @@ export default function FileUploader({ setFile }) {
                 type="file"
                 id="fileInput"
                 onChange={handleFileChange}                        // PARTWISE XML ONLY FOR OSMD (if we want to let them upload xml)
-                accept="image/*, application/pdf, application/xml, application/vnd.recordare.musicxml+xml, .mxl"
+                accept="image/*, application/pdf, application/xml, application/vnd.recordare.musicxml+xml"
                 style={{ display: "none" }}
             />
             <button  className="w-fit mt-4 mb-10 px-3 py-2 text-body border-2 border-black bg-white text-black rounded hover:bg-gray-400 duration-300"
