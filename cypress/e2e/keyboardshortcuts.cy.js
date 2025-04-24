bbd8d72d1f5ea9f1b7174e8ab09c5a15212a76d6
Simeon <121066986+simchildress@users.noreply.github.com>
2025-04-23 21:00:33 -0500

    Added prewarming to pages and used cy.location for no timeout

--- cypress/e2e/keyboardshortcuts.cy.js
+++ cypress/e2e/keyboardshortcuts.cy.js
@@ -1,8 +1,20 @@
 describe('Keyboard Shortcut Navigation', () => {
   const homepage = 'http://localhost:3000/';
-  const lessonsPage = 'http://localhost:3000/Lessons';
-  const sheetMusicPage = 'http://localhost:3000/SheetMusicTools';
-  const communityPage = 'http://localhost:3000/Community';
+  const lessonsPath = '/Lessons';
+  const sheetMusicPath = '/SheetMusicTools';
+  const communityPath = '/Community';
+
+  // Pre-warm (cache) the pages twice before the tests run
+  before(() => {
+    const pages = [homepage, lessonsPath, sheetMusicPath, communityPath];
+    pages.forEach((page) => {
+      cy.visit(page);
+    });
+    pages.forEach((page) => {
+      cy.visit(page);
+    });
+    cy.visit(homepage); // Return to home after pre-warming
+  });
 
   beforeEach(() => {
     cy.visit(homepage);
@@ -12,27 +24,27 @@ describe('Keyboard Shortcut Navigation', () => {
     const keys = '{ctrl}{downarrow}';
     console.log(`Pressing keys: ${keys}`);
     cy.get('body').type(keys);
-    cy.url().should('eq', lessonsPage);
+    cy.location('pathname', { timeout: 10000 }).should('eq', lessonsPath);
   });
 
   it('navigates to Sheet Music Tools with ctrl + left arrow', () => {
     const keys = '{ctrl}{leftarrow}';
     console.log(`Pressing keys: ${keys}`);
     cy.get('body').type(keys);
-    cy.url().should('eq', sheetMusicPage);
+    cy.location('pathname', { timeout: 10000 }).should('eq', sheetMusicPath);
   });
 
   it('navigates to Community with ctrl + right arrow', () => {
     const keys = '{ctrl}{rightarrow}';
     console.log(`Pressing keys: ${keys}`);
     cy.get('body').type(keys);
-    cy.url().should('eq', communityPage);
+    cy.location('pathname', { timeout: 10000 }).should('eq', communityPath);
   });
 
   it('navigates back to Home with ctrl + up arrow', () => {
     const keys = '{ctrl}{uparrow}';
     console.log(`Pressing keys: ${keys}`);
     cy.get('body').type(keys);
-    cy.url().should('eq', homepage);
+    cy.location('pathname', { timeout: 10000 }).should('eq', '/');
   });
 });
